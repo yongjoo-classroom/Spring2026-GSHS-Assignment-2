9b3ddb4961752ec708375e600882553be4793f0d
gs25058 <soulahn11@gmail.com>
2026-01-15 17:04:26 +0900

    fisrt try

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,18 +1,31 @@
 import numpy as np
 
-def logistic_regression(x_train: np.ndarray, y_train: np.ndarray, x_test: np.ndarray) -> np.ndarray:
-    '''
-    Implements the logistic regression algorithm.
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
-    Parameters:
-        - x_train: Training features of shape (n_samples, 2).
-                    For this assignment, each training sample has two features: [feature1, feature2]
-        - y_train: Training labels (0/1)
-                    All the predictions will be binary (0 or 1), since it is Logistic Regression.
-        - x_test: Test features of shape (n_samples, 2).
+class Logistic:
+    def __init__(self, lr):
+        self.lr = lr
+        self.w = np.zeros(2)
+        self.b = 0
 
-    Returns:
-        y_pred: Predicted labels for the test set
-    '''
-    # Your code here
-    pass
+    def forward(self, x):
+        return sigmoid(np.dot(x, self.w) + self.b)
+
+    def predict(self, x):
+        return (self.forward(x) >= 0.5).astype(int)
+
+    def train(self, x_t, y_t, epoch):
+        n_samples = len(y_t)
+        for _ in range(epoch):
+            y_hat = self.forward(x_t)
+            grad_w = np.dot(x_t.T, y_hat - y_t) / n_samples
+            grad_b = np.sum(y_hat - y_t) / n_samples
+            self.w -= self.lr * grad_w
+            self.b -= self.lr * grad_b
+
+
+def logistic_regression(x_train,y_train,x_test):
+    model = Logistic(0.01)
+    model.train(x_train, y_train, 50)
+    return model.predict(x_test)
